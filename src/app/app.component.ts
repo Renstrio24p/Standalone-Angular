0aa7965c55e21bdf8a61185ff5646466ea1a7b30
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-06 04:15:46 +0800

    add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import styles from '../sass/modules/app.module.scss'
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class=${styles.container}>
+        <div>
+            <div class=${styles['d-flex-center']}>
+                <h1 class=${styles['font-weight-500']}>{{ message }}</h1>
+                <img class=${styles['angular-img']} src='./src/images/angular.png' alt='angular logo' />
+            </div>
+            <div class=${styles['d-flex-center']}>
+                <p class=${styles['dark-cyan']}>Welcome to Angular with Webpack 5.88!</p>
+                <img class=${styles.webpack} src='./src/images/webpack.png' alt='webpack img'/>
+            </div>
+        </div>
+        <footer>
+           <div class=${styles['d-flex-center']}>
+                <span>Modular Sass</span>
+                <img class=${styles.sass} src='./src/images/sass.png' alt='sass logo' />
+           </div>
+           <div class=${styles['d-flex-center']}>
+            <img class=${styles.github} src='./src/images/github.png' alt='github' />
+            <span>Codespaces</span>
+           </div>
+        </footer>
+    </div>
+  `,
+})
+export class AppComponent {
+  message = 'Standalone';
+}
