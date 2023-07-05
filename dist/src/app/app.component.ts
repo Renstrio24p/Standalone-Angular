101b6f525c17b98e02188d0ef68d6845c3cb3448
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-06 04:54:39 +0800

    deploy

--- dist/src/app/app.component.ts
+++ dist/src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import styles from '../sass/modules/app.module.scss'
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ message }}</h1>
-    <p>Welcome to Angular with Webpack 5.88!</p>
+    <div class=${styles.container}>
+        <div>
+            <div class=${styles['d-flex-center']}>
+                <h1 class=${styles['font-weight-500']}>{{ message }}</h1>
+                <a href='https://angular.io/'><img class=${styles['angular-img']} src='./src/images/angular.png' alt='angular logo' /></a>
+            </div>
+            <div class=${styles['d-flex-center']}>
+                <p class=${styles['dark-cyan']}>Welcome to Angular with Webpack 5.88!</p>
+                <a href='https://webpack.js.org/'><img class=${styles.webpack} src='./src/images/webpack.png' alt='webpack img'/></a>
+            </div>
+            <div class=${styles['d-flex-w-max']}>
+                <button class=${styles.button} (click)="addCount()">Count is {{ count }}</button>
+            </div>
+        </div>
+        <footer>
+           <div class=${styles['d-flex-center']}>
+                <span>Modular Sass</span>
+                <img class=${styles.sass} src='./src/images/sass.png' alt='sass logo' />
+           </div>
+           <div class=${styles['d-flex-center']}>
+            <img class=${styles.github} src='./src/images/github.png' alt='github' />
+            <span>Codespaces</span>
+           </div>
+        </footer>
+    </div>
   `,
 })
 export class AppComponent {
-  message = 'Hello, Angular!';
+  message = 'Standalone';
+
+  count: number = 0;
+
+  addCount() {
+    this.count++;
+  }
 }
+
--- dist/src/index.ts
+++ dist/src/index.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import 'zone.js/dist/zone';
+import './sass/index.scss'
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
