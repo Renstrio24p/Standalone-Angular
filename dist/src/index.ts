bb82447913dea18897fd45e73ccf1a36ddf8246b
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-07-09 05:17:33 +0800

    add bootstrap for angular webpack

--- dist/src/index.ts
+++ dist/src/index.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import 'zone.js/dist/zone';
-import './sass/index.scss'
+import './sass/index.scss';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
