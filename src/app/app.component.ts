799f44bdfccda64bad6777b2aec273962f60a7c3
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-07-08 21:06:06 +0000

    deploy: 233e61ef275d1f626520776e74b3ad2b4c76541e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
-import styles from '../sass/modules/app.module.scss'
+import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './src/app/app.component.html',
-  styleUrls: ['./src/sass/modules/app.module.scss']
+  templateUrl: '../app/app.component.html',
+  styleUrls: ['../sass/modules/app.module.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   message = 'Standalone';
-
   count: number = 0;
 
   addCount() {
     this.count++;
   }
-}
 
+  ngOnInit() {
+    // Initialize Bootstrap JS components
+    const tooltipElements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipElements.forEach((element: Element) => {
+      new bootstrap.Tooltip(element);
+    });
+
+    const popoverElements = document.querySelectorAll('[data-bs-toggle="popover"]');
+    popoverElements.forEach((element: Element) => {
+      new bootstrap.Popover(element);
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +9,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() {} // Add this empty method
+}
